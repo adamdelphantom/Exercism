cc13f107f735a85ae596e13d50292908e34088a6
adamisrusty <adamgutierreztx@gmail.com>
2020-04-30 16:11:26 -0500

    completed leap exercise

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {} is a leap year", year)
+    if year % 4 == 0 {
+        if year % 400 == 0 {
+            return true;
+        } else if year % 100 == 0 {
+            return false;
+        }
+
+        return true;
+    }
+    false
 }
