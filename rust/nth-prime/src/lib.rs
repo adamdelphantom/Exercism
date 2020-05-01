450167c92c79d0f56b351854910a88d8b2f3d8ba
adamisrusty <adamgutierreztx@gmail.com>
2020-05-01 17:01:23 -0500

    added nth-prime and pseudocode for solution

---
+++ rust/nth-prime/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn nth(n: u32) -> u32 {
+    // Iterate upwards collecting primes
+    // in a vector. Until the length of 
+    // the vector equals n. 
+    //
+    // to check for prime:
+    // for i in prime_candidate..2 {
+    //  if prime_candidate % i == 0 {
+    //  not a prime   
+    // }     
+    // if all numbers do not mod 0  then
+    //  prime.push(prime_candidate)
+    // }
+    n
+}
