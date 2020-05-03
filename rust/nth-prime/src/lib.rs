64bd23e1b1e87d2f6c821d0c880a554b6d5fd546
adamisrusty <adamgutierreztx@gmail.com>
2020-05-03 18:14:23 -0500

    still at it

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,28 +1,20 @@
 pub fn nth(n: u32) -> u32 {
-    // Iterate upwards collecting primes
-    // in a vector. Until the length of 
-    // the vector equals n. 
-    let mut primes = vec![];
+    let mut primes: Vec<u32> = vec![];
     let mut prime_candidate = 1;
-    while primes.len() < n as usize{
-        for i in prime_candidate..1 {
-            if prime_candidate % i == 0 {
-                prime_candidate += 1;
-                break
+    while primes.len() < n as usize + 1 {
+        if prime_candidate == 1 {
+            prime_candidate += 1;
+        } else {
+            for i in 2..prime_candidate {
+                if prime_candidate % i == 0 && i != prime_candidate {
+                    prime_candidate += 1;
+                    break
+                }
             }
-            primes.push(prime_candidate)
         }
+        primes.push(prime_candidate);
         prime_candidate += 1;
     }
-    //
-    // to check for prime:
-    // for i in prime_candidate..2 {
-    //  if prime_candidate % i == 0 {
-    //  not a prime   
-    // }     
-    // if all numbers do not mod 0  then
-    //  prime.push(prime_candidate)
-    // }
-
-    primes[n as usize]
+    println!("{}", n as usize);
+    primes[n as usize - 1]
 }
