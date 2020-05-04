0d8a46b2c33fb15b8fd1f92745ed25d7ef2a498d
adamisrusty <adamgutierreztx@gmail.com>
2020-05-03 19:53:01 -0500

    still no dice on nth-prime

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,20 +1,16 @@
 pub fn nth(n: u32) -> u32 {
-    let mut primes: Vec<u32> = vec![];
-    let mut prime_candidate = 1;
-    while primes.len() < n as usize + 1 {
-        if prime_candidate == 1 {
-            prime_candidate += 1;
-        } else {
-            for i in 2..prime_candidate {
-                if prime_candidate % i == 0 && i != prime_candidate {
-                    prime_candidate += 1;
-                    break
-                }
+    let mut prime_counter = 1;
+    let mut prime_candidate: u32 = 2;
+    while prime_counter < n {   
+        for i in 2..prime_candidate {
+            if prime_candidate % i == 0 {
+                prime_candidate += 1;
+                break
             }
         }
-        primes.push(prime_candidate);
         prime_candidate += 1;
+        prime_counter += 1;
     }
-    println!("{}", n as usize);
-    primes[n as usize - 1]
+    prime_candidate
+    // TODO: find out why the big prime hunt is stopping at 20000
 }
