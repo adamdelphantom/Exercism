74d351617d7b0c628d70affb138fd9ab96fdbad7
adamisrusty <adamgutierreztx@gmail.com>
2020-05-05 17:07:06 -0500

    cleaning up sing function to return properly

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,7 +1,48 @@
 pub fn verse(n: u32) -> String {
-    unimplemented!("emit verse {}", n)
+    let mut song = String::new();
+    if n > 0 {
+        for i in n..0 {
+            song.push_str(&sing(i, i - 1));
+        }
+    } else {
+        song.push_str(&sing(n, n));
+    }
+    song
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    let song_line = String::new();
+    if start > 2 && end > 1 {
+        let song_line = (
+            "{} bottles of beer on the wall, {} bottles of beer.
+Take one down and pass it around, {} bottles of beer on the wall.
+",
+            start,
+            start,
+            end,
+        );
+    } else if start == 2 && end == 1 {
+        let song_line = (
+            "{} bottles of beer on the wall, {} bottles of beer.
+Take one down and pass it around, {} bottle of beer on the wall.
+",
+            start,
+            start,
+            end,
+        );
+    } else if start == 1 && end == 0 {
+        let song_line = (
+            "{} bottle of beer on the wall, {} bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+",
+            start,
+            start,
+        );
+    } else if start == 0 && end == 0 {
+        return "No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+"
+        .to_string();
+    }
+    song_line
 }
