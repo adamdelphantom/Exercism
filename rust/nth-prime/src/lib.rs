0be693c883a1badf3fb71a1dca2e8642277b1d51
adamisrusty <adamgutierreztx@gmail.com>
2020-05-04 21:55:33 -0500

    have logic, need to fix perf to hit big prime

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,16 +1,25 @@
 pub fn nth(n: u32) -> u32 {
-    let mut prime_counter = 1;
+    let mut primes: Vec<u32> = Vec::with_capacity(n as usize);
     let mut prime_candidate: u32 = 2;
-    while prime_counter < n {   
-        for i in 2..prime_candidate {
-            if prime_candidate % i == 0 {
+    while primes.len() < n as usize + 1 {
+        let mut is_prime = true;  
+        if prime_candidate == 2 {
+            primes.push(prime_candidate);
+            prime_candidate += 1;
+        } else {
+            for i in 2..prime_candidate{
+                if prime_candidate % i == 0 {
+                    is_prime = false;
+                }
+            }
+            if is_prime == false {
                 prime_candidate += 1;
-                break
+            } else {
+                primes.push(prime_candidate);
+                prime_candidate += 1;
             }
         }
-        prime_candidate += 1;
-        prime_counter += 1;
     }
-    prime_candidate
-    // TODO: find out why the big prime hunt is stopping at 20000
+    primes[n as usize]
+    // TODO: clean up perf
 }
--- rust/nth-prime/tests/nth-prime.rs
+++ rust/nth-prime/tests/nth-prime.rs
@@ -6,15 +6,23 @@ fn test_first_prime() {
 }
 
 #[test]
-#[ignore]
 fn test_second_prime() {
     assert_eq!(np::nth(1), 3);
 }
 
 #[test]
-#[ignore]
 fn test_sixth_prime() {
     assert_eq!(np::nth(5), 13);
+}
+
+#[test]
+fn test_fiftieth_prime() {
+    assert_eq!(np::nth(49), 229);
+}
+
+#[test]
+fn test_hundredth_prime() {
+    assert_eq!(np::nth(99), 541);
 }
 
 #[test]
