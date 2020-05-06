a20fbaad590c2d4911303c320abd27bc49385289
adamisrusty <adamgutierreztx@gmail.com>
2020-05-05 21:50:50 -0500

    logic is present for everything but song function on beer bottles

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,48 +1,35 @@
 pub fn verse(n: u32) -> String {
-    let mut song = String::new();
-    if n > 0 {
-        for i in n..0 {
-            song.push_str(&sing(i, i - 1));
-        }
+    // need to create &str with string literals then append them to the String
+    let mut line = String::new();
+    if n > 2 {
+        // TODO implement interpolated strings
+        let mut new_line = format!("{} bottles of beer on the wall, {} bottles of beer.
+Take one down and pass it around, {} bottles of beer on the wall.
+", n, n, n - 1);
+        line.push_str(&new_line);
+    } else if n == 2 {
+        line.push_str("2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+");
+    } else if n == 1 {
+        line.push_str("1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+");
     } else {
-        song.push_str(&sing(n, n));
+        line.push_str(
+            "No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+",
+        );
     }
-    song
+    line
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    let song_line = String::new();
-    if start > 2 && end > 1 {
-        let song_line = (
-            "{} bottles of beer on the wall, {} bottles of beer.
-Take one down and pass it around, {} bottles of beer on the wall.
-",
-            start,
-            start,
-            end,
-        );
-    } else if start == 2 && end == 1 {
-        let song_line = (
-            "{} bottles of beer on the wall, {} bottles of beer.
-Take one down and pass it around, {} bottle of beer on the wall.
-",
-            start,
-            start,
-            end,
-        );
-    } else if start == 1 && end == 0 {
-        let song_line = (
-            "{} bottle of beer on the wall, {} bottle of beer.
-Take it down and pass it around, no more bottles of beer on the wall.
-",
-            start,
-            start,
-        );
-    } else if start == 0 && end == 0 {
-        return "No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
-"
-        .to_string();
+    let mut song = String::new();
+    for i in start..end {  
+        println!("{}", &verse(i));   
+        song.push_str(&verse(i));
     }
-    song_line
+    song
 }
