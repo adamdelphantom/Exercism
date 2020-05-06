5c18dd2f50962185585ed7c8bf73fc15173476e7
adamisrusty <adamgutierreztx@gmail.com>
2020-05-05 23:14:58 -0500

    completed logical pass. could still use some DRYing of the text

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,20 +1,27 @@
 pub fn verse(n: u32) -> String {
-    // need to create &str with string literals then append them to the String
     let mut line = String::new();
     if n > 2 {
-        // TODO implement interpolated strings
-        let mut new_line = format!("{} bottles of beer on the wall, {} bottles of beer.
+        let new_line = format!(
+            "{} bottles of beer on the wall, {} bottles of beer.
 Take one down and pass it around, {} bottles of beer on the wall.
-", n, n, n - 1);
+",
+            n,
+            n,
+            n - 1
+        );
         line.push_str(&new_line);
     } else if n == 2 {
-        line.push_str("2 bottles of beer on the wall, 2 bottles of beer.
+        line.push_str(
+            "2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.
-");
+",
+        );
     } else if n == 1 {
-        line.push_str("1 bottle of beer on the wall, 1 bottle of beer.
+        line.push_str(
+            "1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall.
-");
+",
+        );
     } else {
         line.push_str(
             "No more bottles of beer on the wall, no more bottles of beer.
@@ -27,9 +34,11 @@ Go to the store and buy some more, 99 bottles of beer on the wall.
 
 pub fn sing(start: u32, end: u32) -> String {
     let mut song = String::new();
-    for i in start..end {  
-        println!("{}", &verse(i));   
+    for i in (end..=start).rev() {
         song.push_str(&verse(i));
+        if i > end {
+            song.push_str("\n");
+        }
     }
     song
 }
