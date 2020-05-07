9adbdeccd65338e495d08fc10981310dd87b886e
adamisrusty <adamgutierreztx@gmail.com>
2020-05-06 19:40:36 -0500

    completed proverb exercise

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    let mut proverb = String::new();
+    if !list.is_empty() {
+        for (i, x) in list.iter().enumerate() {
+            if i + 1 == list.len() {
+                break;
+            }
+            let line = format!("For want of a {} the {} was lost.\n", x, list[i + 1]);
+            proverb.push_str(&line);
+        }
+        let last_line = format!("And all for the want of a {}.", list[0]);
+        proverb.push_str(&last_line);
+    }
+    proverb
 }
