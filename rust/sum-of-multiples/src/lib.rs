9beba1962ae639c9e0129b7d35fe265c06875c3c
adamisrusty <adamgutierreztx@gmail.com>
2020-05-10 12:32:56 -0500

    solved sum of multiples

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,4 +1,15 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    
-    limit + factors[0]
+    let mut limited_factors = vec![];
+    for factor in factors {
+        for i in 1..limit {
+            if !limited_factors.contains(&(i * factor)) {
+                if i * factor < limit {
+                    limited_factors.push(i * factor);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    limited_factors.iter().sum()
 }
