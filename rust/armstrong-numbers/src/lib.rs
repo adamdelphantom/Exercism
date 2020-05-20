46c6d2d1b995e128321951d6eb5228580bf1f53a
adamisrusty <adamgutierreztx@gmail.com>
2020-05-20 12:05:09 -0500

    first solution pass for armstrong numbers exercise

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,19 @@
+fn number_to_vec(num: u32) -> Vec<u32> {
+    num.to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect()
+}
+
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let digits = number_to_vec(num);
+    let power = digits.len() as u32;
+    let mut sum: u32 = 0;
+    for i in digits {
+        sum += i.pow(power)
+    }
+    if sum == num {
+        return true;
+    }
+    false
 }
