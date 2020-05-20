15c9beeccc7940567772f38b4cc17a19d8daab90
adamisrusty <adamgutierreztx@gmail.com>
2020-05-19 19:08:20 -0500

    added grapheme handling to reverse-string

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,7 +1,5 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    let mut new_string = String::new();
-    for c in input.chars().rev() {
-        new_string.push(c);
-    }
-    new_string
+    input.graphemes(true).rev().collect()
 }
